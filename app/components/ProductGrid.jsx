68be63268552b97426f478eb1c66c3f811155c19
omegamaster86 <masya868686@gmail.com>
2023-09-16 21:32:16 +0900

    add : Pagination

--- app/components/ProductGrid.jsx
+++ app/components/ProductGrid.jsx
@@ -1,13 +1,17 @@
+import {Pagination} from '@shopify/hydrogen';
 import ProductCard from './ProductCard';
 
-export default function ProductGrid({collection, url}) {
-
+export default function ProductGrid({collection}) {
   return (
     <section className="w-full gap-4 md:gap-8 grid">
       <div className="grid-flow-row grid gap-2 gap-y-6 md:gap-4 lg:gap-6 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {collection.products.nodes.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
+        <Pagination connection={collection.products}>
+          {({nodes}) => {
+            return nodes.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ));
+          }}
+        </Pagination>
       </div>
     </section>
   );
