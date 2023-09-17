865d02534c0a388ace322661e0454bd9e92a4f0d
omegamaster86 <masya868686@gmail.com>
2023-09-17 17:54:17 +0900

    add : 製品データを確認する

--- app/routes/products.$handle.jsx
+++ app/routes/products.$handle.jsx
@@ -1,24 +1,43 @@
 import {useLoaderData} from '@remix-run/react';
 import {json} from '@shopify/remix-oxygen';
 
-export function loader({params}) {
+export async function loader({params, context}) {
   const {handle} = params;
+  const {product} = await context.storefront.query(PRODUCT_QUERY, {
+    variables: {
+      handle,
+    },
+  });
+
+  if (!product?.id) {
+    throw new Response(null, {status: 404});
+  }
 
   return json({
-    handle
+    handle,
+    product,
   });
-}
+};
 // このコード例では、ローダー関数に渡されるURL ハンドル変数を paramsから取得し、
 // JSX コンポーネントで使用できるサンプル JSON を返す
 
 export default function ProductHandle() {
-  const {handle} = useLoaderData();
+  const {handle, product} = useLoaderData();
 
-  // ローダーデータをレンダリング
   return (
     <div className="product-wrapper">
       <h2>Product Handle: {handle}</h2>
+      <PrintJson data={product} />
     </div>
+  );
+}
+
+function PrintJson({data}) {
+  return (
+    <details className="outline outline-2 outline-blue-300 p-4 my-2">
+      <summary>Product JSON</summary>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </details>
   );
 }
 
