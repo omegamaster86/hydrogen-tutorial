b714854caeec6f9355ce0b9757977baf289b0ee7
omegamaster86 <masya868686@gmail.com>
2023-09-17 18:21:31 +0900

    add : 商品データをレンダリング

--- app/routes/products.$handle.jsx
+++ app/routes/products.$handle.jsx
@@ -1,5 +1,6 @@
 import {useLoaderData} from '@remix-run/react';
 import {json} from '@shopify/remix-oxygen';
+import {MediaFile} from '@shopify/hydrogen-react';
 
 export async function loader({params, context}) {
   const {handle} = params;
@@ -22,24 +23,45 @@ export async function loader({params, context}) {
 // JSX コンポーネントで使用できるサンプル JSON を返す
 
 export default function ProductHandle() {
-  const {handle, product} = useLoaderData();
+  const {product} = useLoaderData();
 
   return (
-    <div className="product-wrapper">
-      <h2>Product Handle: {handle}</h2>
-      <PrintJson data={product} />
-    </div>
+    <section className="w-full gap-4 md:gap-8 grid px-6 md:px-8 lg:px-12">
+      <div className="grid items-start gap-6 lg:gap-20 md:grid-cols-2 lg:grid-cols-3">
+        <div className="grid md:grid-flow-row  md:p-0 md:overflow-x-hidden md:grid-cols-2 md:w-full lg:col-span-2">
+          <div className="md:col-span-2 snap-center card-image aspect-square md:w-full w-[80vw] shadow rounded">
+            <h2>TODO Product Gallery</h2>
+          </div>
+        </div>
+        <div className="md:sticky md:mx-auto max-w-xl md:max-w-[24rem] grid gap-8 p-0 md:p-6 md:px-0 top-[6rem] lg:top-[8rem] xl:top-[10rem]">
+          <div className="grid gap-2">
+            <h1 className="text-4xl font-bold leading-10 whitespace-normal">
+              {product.title}
+            </h1>
+            <span className="max-w-prose whitespace-pre-wrap inherit text-copy opacity-50 font-medium">
+              {product.vendor}
+            </span>
+          </div>
+          <h3>Product Options TODO</h3>
+          <div
+            className="prose border-t border-gray-200 pt-6 text-black text-md"
+            dangerouslySetInnerHTML={{ __html: product.descriptionHtml }}
+          ></div>
+        </div>
+      </div>
+    </section>
   );
 }
 
-function PrintJson({data}) {
-  return (
-    <details className="outline outline-2 outline-blue-300 p-4 my-2">
-      <summary>Product JSON</summary>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </details>
-  );
-}
+
+// function PrintJson({data}) {
+//   return (
+//     <details className="outline outline-2 outline-blue-300 p-4 my-2">
+//       <summary>Product JSON</summary>
+//       <pre>{JSON.stringify(data, null, 2)}</pre>
+//     </details>
+//   );
+// }
 
 const PRODUCT_QUERY = `#graphql
   query product($handle: String!) {
