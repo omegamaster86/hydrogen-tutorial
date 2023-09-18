05c8f511d5c94c1dba1d4c85086c06e955968e2c
omegamaster86 <masya868686@gmail.com>
2023-09-18 17:01:17 +0900

    add : useSearchParams

--- app/routes/ProductOptions.jsx
+++ app/routes/ProductOptions.jsx
@@ -1,18 +1,20 @@
-import {Link, useLocation} from '@remix-run/react';
-
+import {Link, useLocation,useSearchParams} from '@remix-run/react';
 
 export default function ProductOptions({options}) {
-  // pathname and search will be used to build option URLs
   const {pathname, search} = useLocation();
+  const [currentSearchParams] = useSearchParams();
+
+  const searchParams = currentSearchParams;
 
   return (
     <div className="grid gap-4 mb-6">
-
-      {/* Each option will show a label and option value <Links> */}
       {options.map((option) => {
         if (!option.values.length) {
           return;
         }
+
+        // get the currently selected option value
+        const currentOptionVal = searchParams.get(option.name);
         return (
           <div
             key={option.name}
@@ -24,9 +26,8 @@ export default function ProductOptions({options}) {
 
             <div className="flex flex-wrap items-baseline gap-4">
               {option.values.map((value) => {
-                // Build a URLSearchParams object from the current search string
-                const linkParams = new URLSearchParams(search);
-                // Set the option name and value, overwriting any existing values
+                const linkParams = new URLSearchParams(searchParams);
+                const isSelected = currentOptionVal === value;
                 linkParams.set(option.name, value);
                 return (
                   <Link
@@ -34,7 +35,9 @@ export default function ProductOptions({options}) {
                     to={`${pathname}?${linkParams.toString()}`}
                     preventScrollReset
                     replace
-                    className="leading-none py-1 border-b-[1.5px] cursor-pointer transition-all duration-200"
+                    className={`leading-none py-1 border-b-[1.5px] cursor-pointer transition-all duration-200 ${
+                      isSelected ? 'border-gray-500' : 'border-neutral-50'
+                    }`}
                   >
                     {value}
                   </Link>
