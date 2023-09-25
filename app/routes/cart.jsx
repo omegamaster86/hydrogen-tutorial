bd024bbacadff9620b6e11868a4875a187ac9c1b
omegamaster86 <masya868686@gmail.com>
2023-09-25 19:37:05 +0900

    add : first commit

---
+++ app/routes/cart.jsx
@@ -0,0 +1,50 @@
+import {Link, useLoaderData} from '@remix-run/react';
+import {json} from '@shopify/remix-oxygen';
+import {CartForm} from '@shopify/hydrogen';
+
+export async function action({request, context}) {
+  const {cart} = context;
+
+  const formData = await request.formData();
+  const {action, inputs} = CartForm.getFormInput(formData);
+
+  let result;
+
+  switch(action) {
+    case CartForm.ACTIONS.LinesAdd:
+      result = await cart.addLines(inputs.lines);
+      break;
+    case CartForm.ACTIONS.LinesUpdate:
+      result = await cart.updateLines(inputs.lines);
+      break;
+    case CartForm.ACTIONS.LinesRemove:
+      result = await cart.removeLines(inputs.lineIds);
+      break;
+    default:
+      invariant(false, `${action} cart action is not defined`);
+  }
+
+  // The Cart ID might change after each mutation, so update it each time.
+  const headers = cart.setCartId(result.cart.id);
+
+  return json(
+    result,
+    {status: 200, headers},
+  );
+}
+
+export default function Cart() {
+  return (
+    <div className="flex flex-col space-y-7 justify-center items-center md:py-8 md:px-12 px-4 py-6 h-screen">
+      <h2 className="whitespace-pre-wrap max-w-prose font-bold text-4xl">
+        Your cart is empty
+      </h2>
+      <Link
+        to="/"
+        className="inline-block rounded-sm font-medium text-center py-3 px-6 max-w-xl leading-none bg-black text-white w-full"
+      >
+        Continue shopping
+      </Link>
+    </div>
+  );
+}
